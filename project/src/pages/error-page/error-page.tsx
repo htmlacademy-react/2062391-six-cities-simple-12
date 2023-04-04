fcf52f9241a2e277cb08a5610036cccd37b962ae
Sergey Kocharyan <loki.dogmat@mail.ru>
2023-04-04 11:03:09 +0300

    Маршрут по тайным тропам

---
+++ project/src/pages/error-page/error-page.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+
+function ErrorPage () {
+  return (
+    <div className="page-error">
+      <img className="error-page_img" src="img/error-page.png" alt="404 page"/>
+      <h1 className="cantainer-error_page">
+        Упс .. Такой страницы не существует
+      </h1>
+      <Link to="/"><button className="error-to-main">Вернуться на главную страницу</button> </Link>
+    </div>
+  );
+}
+
+export default ErrorPage;
